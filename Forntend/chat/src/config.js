60aa87c0212a45950776a339d8ba7defdfce4a50
sayanbasani <sayanbasani1@gmail.com>
2025-04-07 19:23:29 +0530

    backend url

--- Forntend/chat/src/config.js
+++ Forntend/chat/src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:3001/";
+const API_BASE_URL = "https://chat-tup4.onrender.com";
+// const API_BASE_URL = "http://localhost:3001/";
 // http://localhost:3001
 export default API_BASE_URL;
