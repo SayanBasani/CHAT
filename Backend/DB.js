b07eabf06d8d1bec02e680a5f0064450744d5d62
sayanbasani <sayanbasani1@gmail.com>
2025-04-07 20:37:19 +0530

    dotenv

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,7 +1,7 @@
 import e from "express";
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
-
+dotenv.config(); 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
