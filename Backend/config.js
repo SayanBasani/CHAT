5029e7dec3c3149835d9dd185ad655406dcfce90
Sayan Basani <142533918+SayanBasani@users.noreply.github.com>
2025-04-19 20:17:52 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const FORNTEND_BASE_URL = "http://localhost:5173";
-// const FORNTEND_BASE_URL = process.env.FRONTEND_BASE_URL || "https://chat-forntent.onrender.com";
+// const FORNTEND_BASE_URL = "http://localhost:5173";
+const FORNTEND_BASE_URL = process.env.FRONTEND_BASE_URL || "https://chat-forntent.onrender.com";
 
 export default FORNTEND_BASE_URL;
